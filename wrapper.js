9daca01e9d211733d6cf7ecbbf5de1b51b5eac7b
AndreJesusBrito <35274086+AndreJesusBrito@users.noreply.github.com>
2018-09-29 09:27:38 +0100

    add wrapper base

--- wrapper.js
+++ wrapper.js
@@ -5,11 +5,93 @@ class DiscordWrapper {
 
     }
 
+    //-------------------------------------
+    // Recive
+    //-------------------------------------
+
     onMsg() {
 
     }
 
-    sendMsg() {
+    onHello() {
+
+    }
+
+    onReady() {
+
+    }
+
+    onResumed() {
+
+    }
+
+    onInvalidSession() {
+
+    }
+
+    // channel Handlers
+    onChannelCreate() {
+
+    }
+    onChannelUpdate() {
+
+    }
+    onChannelDelete() {
+
+    }
+    onChannelPinsUpdate() {
+
+    }
+
+
+    // Guild Handlers
+    onGuildCreate() {
+
+    }
+    onGuildUpdate() {
+
+    }
+    onGuildDelete() {
+
+    }
+    onGuildBanAdd() {
+
+    }
+    onGuildBanRemove() {
+
+    }
+
+
+    //-------------------------------------
+    // send
+    //-------------------------------------
+
+    sendMsg(msg, target) {
+
+    }
+
+    setState() {
+
+    }
+
+    setUsername() {
+
+    }
+
+    setAvatar() {
+
+    }
+
+    setAttachment() {
+
+    }
+
+    setUserNickName(user, newNick) {
+
+    }
+
+
+    setUserRole(user, role) {
 
     }
 }
