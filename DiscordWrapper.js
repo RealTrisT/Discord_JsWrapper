314074462a8c1c8edb09d899e1e2a400249a6c5b
TrisT <30376152+RealTrisT@users.noreply.github.com>
2018-09-29 13:36:32 +0000

    Guild Caching and attempt at getting emoji fucking urls

--- DiscordWrapper.js
+++ DiscordWrapper.js
@@ -3,6 +3,13 @@ class DiscordWrapper{
 	constructor(Token_){
 		this.events 	= new (require('events'))();
 		this.ApiWrapper = new (require('./DiscordWrapperApi'))(Token_, {obj: this.events});
+
+		this.GuildCache = new Array();
+		console.log(this.GuildCache);
+
+		//All Gateway Events: https://discordapp.com/developers/docs/topics/gateway#commands-and-events-gateway-events
+		this.events.me = this; //HACK THAT SHIT LMAO
+		this.events.on('GUILD_CREATE', this.GUILD_CREATE_handler);
 	}
 
 	async Initialize(){
@@ -10,8 +17,22 @@ class DiscordWrapper{
 		await this.ApiWrapper.networking.OpenGateway();
 	}
 
+
+	//https://discordapp.com/developers/docs/resources/guild#guild-object
+	GUILD_CREATE_handler(data){
+		this.me.GuildCache.push(data);
+	}
+	GetCachedGuild(GuildID){
+		console.log("cached guilds: " + this.GuildCache.length);
+		for (var i = 0; i < this.GuildCache.length; i++){
+			console.log(this.GuildCache[i].name + ": ", this.GuildCache[i].id)
+			if(this.GuildCache[i].id == GuildID)return this.GuildCache[i];
+		}
+		return undefined;
+	}
+
 	//https://discordapp.com/developers/docs/resources/channel#create-message
-	async CreateMessage(channelID, message){
+	async CreateMessage(channelID, message){//message is text
 		return await this.ApiWrapper.networking.HttpApiSend(
 			"POST", 
 			"/channels/" + channelID + "/messages", 
@@ -36,4 +57,16 @@ class DiscordWrapper{
 			"/channels/" + channelID + "/messages" + ((queryParams != "")?("?" + queryParams.substring(1)):(""))
 		);
 	}
+
+	async GetUser(userID){
+		return await this.ApiWrapper.networking.HttpApiSend(
+			"GET",
+			"/users/" + userID
+		);
+	}
 }
+
+
+
+//--------------------------------------------------------------------------------------------------
+//https://discordapp.com/developers/docs/resources/channel#message-object
--- test.js
+++ test.js
@@ -5,6 +5,9 @@ auth = require('./auth.json');
 async function ey(){
 	var IOs = new DiscordWrapper(auth.token);
 
+	var uncucked = {'123': {streng: 'gay'}, '321': {streng: 'gay2'}};
+	console.log(uncucked['123'].streng);
+
 	IOs.events.on('MESSAGE_CREATE', async function(data){
 		if(data.content.substring(0, 1) == '!'){
 			var endCmd = data.content.indexOf(' ');
@@ -16,12 +19,32 @@ async function ey(){
 				IOs.CreateMessage(data.channel_id, "https://github.com/RealTrisT/Discord_JsWrapper");
 			}else if(command == 'elpb4'){
 				previousMessages = JSON.parse(await IOs.GetMessages(data.channel_id, {'before': data.id, 'limit': 1}));
-				//console.log("PREVMESSAGE(" + previousMessage.length + "): " + JSON.stringify(previousMessage);
 				IOs.DeleteMessage(data.id, data.channel_id);
 				IOs.CreateMessage(data.channel_id, 
 					"https://www.google.pt/search?q=" + encodeURIComponent(previousMessages[0].content) + " " + 
 					"<@!" + previousMessages[0].author.id + ">"
 				);
+			}else if(command =='emoju'){//this does not work, I do not know how to get the url of the fucking thing
+				var emojin = data.content.substring(7);
+
+				var found = false;
+				var daguild = IOs.GetCachedGuild(data.guild_id);
+				if(!daguild)return;
+				emojiarr = daguild.emojis;
+				for (var i = 0; i < emojiarr.length; i++) {
+					if(emojiarr[i].name == emojin){
+						console.log("getting :" + emojin + ":");
+						found = true;
+
+						var gotuser = await IOs.GetUser(emojiarr[i].id);
+						console.log("user thing: " + gotuser);
+						IOs.CreateMessage(
+							data.channel_id, 
+							"https://cdn.discordapp.com/emojis/" + JSON.parse(gotuser).avatar + ".png" 
+						);
+						break;
+					}
+				}
 			}
 //----------------------------------------------------------------				
 		}
